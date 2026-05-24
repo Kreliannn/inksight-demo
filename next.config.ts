bf62aab9684a01693ff3e4a6953c7edf0ca3bd25
Kreliannn <krelianquimson@gmail.com>
2026-05-24 12:15:08 +0800

    udpae

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
-
-} as any;
+};
 
 export default nextConfig;
